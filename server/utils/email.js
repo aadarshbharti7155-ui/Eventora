59e0c1a6ef33ad11c487bf429467dcbb890b2dbb
Aadarsh <aadarshbharti7155@gmail.com>
2026-06-19 23:37:11 +0530

    fixed nodemailer

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,7 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // Use SSL/TLS
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
