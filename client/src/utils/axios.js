e83d693a5e9eeb3fa90c320b58678d7a16a997cd
Aadarsh <aadarshbharti7155@gmail.com>
2026-06-19 16:53:17 +0530

    Fixed axios baseURL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://eventora-backend-ilbp.onrender.com',
+    baseURL: 'https://eventora-backend-ilbp.onrender.com/api',
     headers:{
         'Content-Type' : 'application/json'
     },
