af68ffa24b5cf0c657a344d61c3c2639e1a116bf
Aadarsh <aadarshbharti7155@gmail.com>
2026-06-20 19:22:15 +0530

    Update client API URL to production backend

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'eventora-h6x8.vercel.app/api',
+    baseURL: 'eventora-h6x8.vercel.app',
     headers:{
         'Content-Type' : 'application/json'
     },
