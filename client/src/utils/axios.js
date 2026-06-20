9076675b8339087355466c8b94c86c13d1a55daa
Aadarsh <aadarshbharti7155@gmail.com>
2026-06-20 19:35:14 +0530

    Fix API baseURL to include https://

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'eventora-h6x8.vercel.app',
+    baseURL: 'https://eventora-h6x8.vercel.app/api',
     headers:{
         'Content-Type' : 'application/json'
     },
